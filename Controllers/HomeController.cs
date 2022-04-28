5258629f03a27f45f77616976f0d1db14ed6a529
Shihan Mohamed <shihanmails@gmail.com>
2022-04-28 23:46:00 +0530

    Homepage

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BurgerShop.Models;
+using BurgerShop.ViewModels;
+
+namespace BurgerShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IBurgerRepository _burgerRepository;
+
+        public HomeController(IBurgerRepository burgerRepository)
+        {
+            _burgerRepository = burgerRepository;
+        }
+        public IActionResult Index()
+        {
+            var homeViewModel = new HomeViewModel
+            {
+                BurgerOfTheWeek = _burgerRepository.BurgersOfTheWeek
+            };
+            return View(homeViewModel);
+        }
+    }
+}
+
---
+++ ViewModels/HomeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BurgerShop.Models;
+
+namespace BurgerShop.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Burger> BurgerOfTheWeek { get; set; }
+    }
+}
